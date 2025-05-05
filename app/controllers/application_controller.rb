577409f6aec4fe755347242fa3f1cee8fba6252c
mofumofu8Y <yamamo1995y@gmail.com>
2025-05-05 17:04:02 +0900

    データベースブランチ作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+
+  before_action :basic_auth
+
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'yamamo2020y@gmail.com' && password == 'pamyupamyu8Y'
+    end
+  end
 end
+
+
