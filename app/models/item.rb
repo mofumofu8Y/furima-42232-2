b8aa56024262b25c6fe0ce7920ddf76d151452c8
mofumofu8Y <yamamo1995y@gmail.com>
2025-05-13 16:41:35 +0900

    修正追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
-  has_one :order 
+  # has_one :order 
 
   # ActiveHashを使ったアソシエーション
   extend ActiveHash::Associations::ActiveRecordExtensions
