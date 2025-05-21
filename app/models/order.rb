f4551516e82d8dd757e70b4a24412f34fc59f87a
mofumofu8Y <yamamo1995y@gmail.com>
2025-05-21 16:41:00 +0900

    修正④

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
  class Order < ApplicationRecord
    belongs_to :item
    belongs_to :user
-
+   has_one :shipping_address
 
 #   validates :address, presence: true
 #   validates :payment_method, presence: true
