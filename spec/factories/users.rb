0fd6b1bf28687e7ea384ce0df77b4f9a27f071ca
mofumofu8Y <yamamo1995y@gmail.com>
2025-05-22 00:10:02 +0900

    Update users.rb

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -9,8 +9,6 @@ FactoryBot.define do
     last_name_kana        { 'ヤマダ' }
     first_name_kana       { 'タロウ' }
     birth_date            { '1990-01-01' }
-    has_many :orders
-  has_many :items
 
   end
 end
