33754ac530762074fda1d2d267e78e89243c20b8
mofumofu8Y <yamamo1995y@gmail.com>
2025-05-22 17:09:46 +0900

    修正

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,35 +1,39 @@
-const pay = () => {
-  const publicKey = gon.public_key;
-  const payjp = Payjp(publicKey);
-  const elements = payjp.elements();
+document.addEventListener("DOMContentLoaded", () => {
 
-  const numberElement = elements.create('cardNumber');
-  const expiryElement = elements.create('cardExpiry');
-  const cvcElement = elements.create('cardCvc');
+  const pay = () => {
+    console.log("OK")
 
-  numberElement.mount('#number-form');
-  expiryElement.mount('#expiry-form');
-  cvcElement.mount('#cvc-form');
+    const publicKey = gon.public_key;
+    const payjp = Payjp(publicKey);
+    const elements = payjp.elements();
+    const numberElement = elements.create('cardNumber');
+    const expiryElement = elements.create('cardExpiry');
+    const cvcElement = elements.create('cardCvc');
 
-  const form = document.getElementById('charge-form');
-  form.addEventListener("submit", (e) => {
-    payjp.createToken(numberElement).then(function (response) {
-      if (response.error) {
-      } else {
-        const token = response.id;
-        const renderDom = document.getElementById("charge-form");
-        const tokenObj = `<input value=${token} name='token' type="hidden">`;
-        renderDom.insertAdjacentHTML("beforeend", tokenObj);
-      }
-      numberElement.clear();
-      expiryElement.clear();
-      cvcElement.clear();
-      document.getElementById("charge-form").submit();
+    numberElement.mount('#number-form');
+    expiryElement.mount('#expiry-form');
+    cvcElement.mount('#cvc-form');
+
+    const form = document.getElementById('charge-form');
+    form.addEventListener("submit", (e) => {
+      payjp.createToken(numberElement).then(function (response) {
+        if (response.error) {
+        } else {
+          const token = response.id;
+          const renderDom = document.getElementById("charge-form");
+          const tokenObj = `<input value=${token} name='token' type="hidden">`;
+          renderDom.insertAdjacentHTML("beforeend", tokenObj);
+        }
+        numberElement.clear();
+        expiryElement.clear();
+        cvcElement.clear();
+        document.getElementById("charge-form").submit();
+      });
+      e.preventDefault();
     });
-    e.preventDefault();
-  });
-};
-
+  };
+  pay();
+});
 // turbo:loadとturbo:renderの両方に対応
-window.addEventListener("turbo:load", pay);
-window.addEventListener("turbo:render", pay);
+// window.addEventListener("turbo:load", pay);
+// window.addEventListener("turbo:render", pay);
